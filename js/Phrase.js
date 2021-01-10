c3fcd53bb2d96e9be8a271c823d16ffca338930e
Manoel Blue <manoelbluee@gmail.com>
2021-01-10 16:18:23 +0100

    add Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    // Adds a placeholder for each letter:
+    addPhraseToDisplay() {
+
+    }
+
+    // Checks if the selected letter is a match:
+    checkLetter() {
+
+    }
+
+    // Reveals the letters when they are a match:
+    showMatchedLetter() {
+        
+    }
+}
