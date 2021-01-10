f7610b3c540c461943e316ac382b7fadc093ea6c
Manoel Blue <manoelbluee@gmail.com>
2021-01-10 17:31:05 +0100

    add buttons listeners

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game;
+const buttonStart = document.getElementById("btn__reset");
+const keyboard = document.getElementById("qwerty");
+
+// Listen to click on Start button
+// then begin the game:
+buttonStart.addEventListener("click", (e) => {
+    game = new Game();
+    game.startGame();
+})
+
+// Listen to click on keyboard buttons:
+keyboard.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+        console.log(e.target);
+
+        game.handleInteraction();
+    }
+})
