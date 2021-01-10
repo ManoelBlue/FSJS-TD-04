5b665bad80d57bef4b62c08c1014f1c3c2d5cb4e
Manoel Blue <manoelbluee@gmail.com>
2021-01-10 16:44:43 +0100

    add Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,54 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0,
+        this.phrases = [
+            'Phrase',
+            'Phrase',
+            'Phrase',
+            'Phrase',
+            'Phrase'
+        ],
+        this.activePhrase = null
+    }
+
+    // Iniates the game:
+    startGame() {
+
+    };
+
+    // Randomly choses a phrase and returns it:
+    getRandomPhrase() {
+
+    };
+
+    // Adds logic to user interaction:
+    handleInteraction() {
+
+        // Disables the selected letter’s onscreen keyboard
+        
+        // Handles wrong matches:
+
+        // Handles the correct matches:
+
+
+    };
+
+    // Removes one life:
+    removeLife() {
+
+    };
+
+    // Checks if the user has guessed the phrase:
+    removeLife() {
+
+    };
+
+    // Finishes the game:
+    gameOver() {
+
+    };
+}
